66f3315e4635f463346cc01ae5366c34dfa27a47
DeemaJS <deeema@outlook.com>
2016-07-21 17:53:01 +0300

    Add autoprefixer and browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
 	console.log(process.env);
@@ -9,5 +11,20 @@ gulp.task('default', function() {
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browser: ['last 2 version']
+		}))
 		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 8080,
+        open: true,
+        notify: false
+    });
+    browserSync.stream();
 });

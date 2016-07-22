5a50fa2bc2e4c3e2a06b61d121d3aab4dac7aaa4
DeemaJS <deeema@outlook.com>
2016-07-22 15:45:35 +0300

    Eslint plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
+/* eslint-env node */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-
-gulp.task('default', function() {
-	console.log(process.env);
-
-});
+var eslint = require('gulp-eslint');
 
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
@@ -27,4 +25,27 @@ gulp.task('browser-sync', function() {
         notify: false
     });
     browserSync.stream();
+});
+
+gulp.task('lint', function () {
+    
+    return gulp.src(['**/*.js','!node_modules/**'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['styles', 'lint'], function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['lint']);
+	//console.log(process.env);
+	 browserSync.init({
+        server: './'
+     });
 });
